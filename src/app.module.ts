7f407da15ab3511d1f2d2fa6ef9e723a5cf27381
MUHAMMED RAZIN P <muhammed.razin@ultsglobal.com>
2025-04-08 00:55:06 +0530

    Normalize line endings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGOURI'),
+      }),
+      inject: [ConfigService],
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
